f0f96ee48aece886958d4017590adad04b38706a
Liangfei <the.epic.boona@gmail.com>
2026-08-08 16:34:08 -0400

    I removed feedforward because it was unncessary because the linear slides don't fall on their own.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID_Controller.java
@@ -1,20 +1,19 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class PIDF_Controller {
+public class PID_Controller {
 
     /* Not currently sure if I even need Feedforward for the linear slide elevator
        check in with Russell or Ethan later. */
-    private double kP, kI, kD, kF;
+    private double kP, kI, kD;
     private double integral, derivative;
     private double lastError, target, error, output;
     private ElapsedTime timer = new ElapsedTime();
 
-    public PIDF_Controller (double kP, double kI, double kD, double kF) {
+    public PID_Controller (double kP, double kI, double kD) {
         this.kP = kP;
         this.kI = kI;
         this.kD = kD;
-        this.kF = kF;
         timer.reset();
     }
 
@@ -28,7 +27,7 @@ public class PIDF_Controller {
         error = target - current;
         derivative = (error - lastError) / timer.seconds();
         integral += error * timer.seconds();
-        output = (kP * error) + (kI * integral) + (kD * derivative) + kF;
+        output = (kP * error) + (kI * integral) + (kD * derivative);
         lastError = error;
 
         return output;
